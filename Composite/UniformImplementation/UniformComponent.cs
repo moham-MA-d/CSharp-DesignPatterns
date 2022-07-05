cc0cb8bf438677cb640c4480e27f43211b615d6c
mohammadAlvanpoor <mohammad.alvanpoor@gmail.com>
2022-07-05 15:43:31 +0430

    Add Uniform and Safe implementation of Composite Pattern

---
+++ Composite/UniformImplementation/UniformComponent.cs
@@ -0,0 +1,15 @@
+﻿namespace Composite.UniformImplementation;
+
+internal abstract class UniformComponent
+{
+    protected readonly string _name;
+
+    protected UniformComponent(string name)
+    {
+        _name = name;
+    }
+
+    public abstract void Add(UniformComponent c);
+    public abstract void Remove(UniformComponent c);
+    public abstract void Display(int depth);
+}
---
+++ Composite/UniformImplementation/UniformComposite.cs
@@ -0,0 +1,26 @@
+﻿namespace Composite.UniformImplementation;
+internal class UniformComposite : UniformComponent
+{
+    public UniformComposite(string name) : base(name) { }
+
+    private readonly List<UniformComponent> _children = new();
+
+    public override void Add(UniformComponent c)
+    {
+        _children.Add(c);
+    }
+
+    public override void Remove(UniformComponent c)
+    {
+        _children.Remove(c);
+    }
+
+    public override void Display(int depth)
+    {
+        Console.WriteLine(new string('-', depth) + _name);
+        foreach (var c in _children)
+        {
+            c.Display(depth + 2);
+        }
+    }       
+}
