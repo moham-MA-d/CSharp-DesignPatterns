9814c1d4c194c35a4dea7365c1944b6f2e2791b1
mohammadAlvanpoor <mohammad.alvanpoor@gmail.com>
2022-07-06 11:00:59 +0430

    Add Adapter Pattern

---
+++ Adapter/Model/Food.cs
@@ -0,0 +1,7 @@
+﻿namespace Adapter.Model;
+
+public class Food
+{
+    public string Name { get; set; }
+    public int Price { get; set; }
+}
---
+++ Adapter/Program.cs
@@ -0,0 +1,12 @@
+﻿using Adapter.Adapter;
+using Adapter.Repository;
+using Adapter.Service;
+
+Console.WriteLine("====================Foods From File====================");
+const string filename = @"../../../../Adapter/Foods.json";
+var fileService = new FoodService(new FoodAdapterFile(filename, new FoodRepositoryFile()));
+Console.WriteLine(fileService.ListCharacters());
+
+Console.WriteLine("====================Foods From Database====================");
+var dbService = new FoodService(new FoodAdapterDb(new FoodRepositoryDb()));
+Console.WriteLine(dbService.ListCharacters());
