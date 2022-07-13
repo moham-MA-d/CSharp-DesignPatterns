e0be98b6b8bddf7e5c83d58e8fe35a80e26b5ac3
mohammadAlvanpoor <mohammad.alvanpoor@gmail.com>
2022-07-13 23:47:59 +0430

    Add Singleton Pattern

---
+++ Singleton/ApplicationUsers.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Singleton;
+
+public sealed class ApplicationUsers 
+{
+    private static volatile ApplicationUsers? _instance;
+    private static readonly object SyncLock = new();
+
+    private ApplicationUsers()
+    {
+    }
+
+    public static ApplicationUsers? Instance
+    {
+        get
+        {
+            if (_instance is not null) return _instance;
+            
+            lock (SyncLock)
+                if (_instance is null) _instance = new ApplicationUsers();
+            
+            return _instance;
+        }
+    }
+
+    public int OnlineUsers { get; set; }
+    public int OfflineUsers { get; set; }
+}
