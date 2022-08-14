f91aff1a8bfccf51fead2f2d7f8982efb1d422c0
mohammadAlvanpoor <mohammad.alvanpoor@gmail.com>
2022-08-14 21:45:11 +0430

    Add TemplateMethod Pattern

---
+++ TemplateMethod/Models/Food.cs
@@ -0,0 +1,28 @@
+﻿using TemplateMethod.TemplateMethod;
+
+namespace TemplateMethod.Models;
+
+public abstract class Food
+{
+    private readonly Restaurant _restaurant;
+    
+    protected Food(){_restaurant = new Restaurant();}
+    protected Food(Restaurant restaurant)
+    {
+        _restaurant = restaurant;
+    }
+
+    public void Order()
+    {
+        _restaurant.OrderFood();
+        
+        PrepareIngredients();
+        Cook();
+        Design();
+    }
+
+    protected abstract void PrepareIngredients();
+    protected abstract void Cook();
+    protected abstract void Design();
+    
+}
